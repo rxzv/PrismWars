f93ae6954ede83398d69b19721883515a3f56890
rxzv <zxcrxzv@gmail.com>
2025-09-22 18:53:17 +0300

    add: network helper

---
+++ Assets/_PrismWars/_Scripts/NetworkHelper/ClientNetworkTransform.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Netcode.Components;
+using UnityEngine;
+
+namespace PrismWars.NetworkHelper {
+    public enum AuthorityMods {
+        Server,
+        Client
+    }
+    
+    [DisallowMultipleComponent]
+    public class ClientNetworkTransform : NetworkTransform {
+        public AuthorityMods authorityMode = AuthorityMods.Client;
+
+        protected override bool OnIsServerAuthoritative() => authorityMode == AuthorityMods.Server;
+    }
+}
