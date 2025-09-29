b89cf2bae258896852e643335c008fa5e9de4eee
rxzv <zxcrxzv@gmail.com>
2025-09-29 15:09:26 +0300

    add: flipX

---
+++ Assets/_PrismWars/_Scripts/Systems/PlayerSystem/FlipXController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace _PrismWars._Scripts.Player {
+    public class FlipXController {
+        SpriteRenderer _spriteRenderer;
+        
+        public FlipXController(SpriteRenderer spriteRenderer) {
+            _spriteRenderer = spriteRenderer;
+        }
+
+        public void FlipX(Vector2 dir) {
+            if (dir.x == 1)
+                _spriteRenderer.flipX = false;
+            else if (dir.x == -1)
+                _spriteRenderer.flipX = true;
+        }
+    }
+}
--- Assets/_PrismWars/_Scripts/Systems/PlayerSystem/PlayerController.cs
+++ Assets/_PrismWars/_Scripts/Systems/PlayerSystem/PlayerController.cs
@@ -1,28 +1,33 @@
+using System;
 using R3;
 using Unity.Netcode;
 using UnityEngine;
 
 namespace _PrismWars._Scripts.Player
 {
-    public class PlayerController : NetworkBehaviour {
+    public class PlayerController : NetworkBehaviour, IDisposable {
         PlayerConfig _config;
         
         MovementController _movementController;
         JumpingController _jumpingController;
+        FlipXController _flipXController;
         
         CompositeDisposable _disposables = new();
 
         Vector3 _direction;
         Rigidbody2D _rb;
+        SpriteRenderer _spriteRenderer;
         
         void Start() {
             if (!IsOwner) return;
 
             _config = ServiceLocator.Current.Get<PlayerConfig>();
             _rb = GetComponent<Rigidbody2D>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
             
             _movementController = new MovementController(transform, _config.MoveSpeed);
             _jumpingController = new JumpingController(_rb, _config.JumpForce);
+            _flipXController = new FlipXController(_spriteRenderer);
             
             ServiceLocator.Current.Get<InputService>().MoveInput
                 .Subscribe(d => _movementController.Move(d))
@@ -30,6 +35,12 @@ namespace _PrismWars._Scripts.Player
             ServiceLocator.Current.Get<InputService>().JumpCommand
                 .Subscribe(_ => _jumpingController.Jump())
                 .AddTo(_disposables);
+            ServiceLocator.Current.Get<InputService>().MoveInput
+                .Subscribe(d => _flipXController.FlipX(d))
+                .AddTo(_disposables);
         }
+
+        public void Dispose() =>
+            _disposables?.Dispose();
     }
 }
