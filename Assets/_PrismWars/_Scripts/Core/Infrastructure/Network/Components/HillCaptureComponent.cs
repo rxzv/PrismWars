64b59068f2cd8b8e8f2fa656c16519e2c8a057ea
rxzv <zxcrxzv@gmail.com>
2025-11-05 23:21:56 +0300

    Update HillCaptureComponent.cs

--- Assets/_PrismWars/_Scripts/Core/Infrastructure/Network/Components/HillCaptureComponent.cs
+++ Assets/_PrismWars/_Scripts/Core/Infrastructure/Network/Components/HillCaptureComponent.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections.Generic;
 using _PrismWars._Scripts.Player;
 using _PrismWars._Scripts.UI.Model;
 using _PrismWars._Scripts.Utils;
@@ -10,27 +8,11 @@ namespace _PrismWars._Scripts.Core.Infrastructure.Network.Components {
     public class HillCaptureComponent : NetworkBehaviour {
         [SerializeField] float _captureDuration = 5f;
         
-        NetworkVariable<PlayerElement> _hillElement = new NetworkVariable<PlayerElement>(PlayerElement.None);
-        Dictionary<ulong, PlayerElement> _hillPlayers = new();
+        NetworkVariable<PlayerElement> _hillElement = new ();
+        NetworkList<int> _hillPlayers = new();
         
-        NetworkVariable<int> _hillPlayersCount = new NetworkVariable<int>();
         Timer _hillCaptureTimer;
         SpriteRenderer _spriteRenderer;
-
-        void OnTriggerEnter2D(Collider2D other) {
-            if(!IsServer) return;
-            if (other.TryGetComponent(out PlayerController player)) {
-                _hillPlayers.Add(player.ClientId, player.PlayerElement.Value);
-                _hillPlayersCount.Value = _hillPlayers.Count;
-            }
-        }
-        void OnTriggerExit2D(Collider2D other) {
-            if(!IsServer) return;
-            if (other.TryGetComponent(out PlayerController player)) {
-                _hillPlayers.Remove(player.ClientId);
-                _hillPlayersCount.Value = _hillPlayers.Count;
-            }
-        }
 
         public override void OnNetworkSpawn() {
             _spriteRenderer = GetComponent<SpriteRenderer>();
@@ -39,45 +21,89 @@ namespace _PrismWars._Scripts.Core.Infrastructure.Network.Components {
             if (IsServer) {
                 _hillCaptureTimer = new Timer();
                 _hillCaptureTimer.OnTimerComplete += OnCaptureComplete;
-                _hillPlayersCount.OnValueChanged += HillPlayersCountChanged;
+                _hillPlayers.OnListChanged += HillPlayersCountChanged;
             }
             
             _hillElement.OnValueChanged += OnHillElementChanged;
             base.OnNetworkSpawn();
         }
 
-        void HillPlayersCountChanged(int previousValue, int newValue) {
-            if (newValue == 0) {
+        void HillPlayersCountChanged(NetworkListEvent<int> changeEvent) {
+            if (!IsServer) return;
+            if (changeEvent.Value == 0) {
                 _hillCaptureTimer.StopTimer();
                 return;
             }
 
             PlayerElement dominantElement = GetDominantElement();
-            
-            if (dominantElement == PlayerElement.None) 
-                _hillCaptureTimer.StopTimer();
-            else 
-                if (!IsTimerRunning())
-                    _hillCaptureTimer.StartTimer(_captureDuration);
+
+            if (dominantElement == PlayerElement.None) {
+                _hillElement.Value = dominantElement;
+                if(IsTimerRunning())
+                    _hillCaptureTimer.StopTimer();
+            }
+            else if (!IsTimerRunning() && _hillElement.Value != dominantElement) {
+                _hillCaptureTimer.StartTimer(_captureDuration);
+            }
         }
+
+        void OnTriggerEnter2D(Collider2D other) {
+            if (!IsServer) return;
+            if (other.TryGetComponent(out PlayerController player)) {
+                _hillPlayers.Add(GetPlayerIntForPlayerElement(player.PlayerElement.Value));
+            }
+        }
+
+        int GetPlayerIntForPlayerElement(PlayerElement playerElement) {
+            return playerElement switch {
+                PlayerElement.None => 0,
+                PlayerElement.Fire => 1,
+                PlayerElement.Ice => 2,
+                _ => -1
+            };
+        }
+        PlayerElement GetPlayerElementForId(int id) {
+            return id switch {
+                0 => PlayerElement.None,
+                1 => PlayerElement.Fire,
+                2 => PlayerElement.Ice,
+                _ => PlayerElement.None
+            };
+        }
+        
+        void OnTriggerExit2D(Collider2D other) {
+            if(!IsServer) return;
+            if (other.TryGetComponent(out PlayerController player)) {
+                var id = GetPlayerIntForPlayerElement(player.PlayerElement.Value);
+                _hillPlayers.Remove(id);
+            }
+            Debug.Log("Exit");
+        }
+
         void OnCaptureComplete() {
             if (!IsServer) return;
+            Debug.Log("OnCaptureComplete");
             
             PlayerElement dominantElement = GetDominantElement();
             if (dominantElement != PlayerElement.None) 
                 _hillElement.Value = dominantElement;
         }
         PlayerElement GetDominantElement() {
-            if (_hillPlayers.Count == 0) return PlayerElement.None;
+            if (_hillPlayers.Count == 0) 
+                return PlayerElement.None;
 
-            PlayerElement? firstElement = null;
-            foreach (var element in _hillPlayers.Values) {
-                if (firstElement == null) 
+            int? firstElement = null;
+    
+            foreach (var element in _hillPlayers) {
+                if (firstElement == null) {
                     firstElement = element;
-                else if (firstElement != element)
+                }
+                else if (firstElement.Value != element) {
                     return PlayerElement.None;
+                }
             }
-            return firstElement.Value;
+    
+            return GetPlayerElementForId(firstElement.Value);
         }
 
         bool IsTimerRunning() => _hillCaptureTimer.GetRemainingTime() > 0;
@@ -97,12 +123,18 @@ namespace _PrismWars._Scripts.Core.Infrastructure.Network.Components {
         }
 
         Color GetColorHillByElement(PlayerElement playerElement) =>
-            playerElement switch
-            {
+            playerElement switch {
                 PlayerElement.None => Color.gray,
                 PlayerElement.Ice => Color.blue,
                 PlayerElement.Fire => Color.red,
                 _ => Color.black
             };
+        public override void OnNetworkDespawn() {
+            if (IsServer) {
+                _hillCaptureTimer.OnTimerComplete -= OnCaptureComplete;
+            }
+            _hillElement.OnValueChanged -= OnHillElementChanged;
+            base.OnNetworkDespawn();
+        }
     }
 }
