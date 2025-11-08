66f03974a420e79b2f379dd744d6adc9cc19495d
rxzv <zxcrxzv@gmail.com>
2025-11-08 19:55:02 +0300

    fix: cat owner by player

--- Assets/_PrismWars/_Scripts/Core/Infrastructure/Network/Components/CaptureTheCatComponent.cs
+++ Assets/_PrismWars/_Scripts/Core/Infrastructure/Network/Components/CaptureTheCatComponent.cs
@@ -4,89 +4,75 @@ using UnityEngine;
 
 namespace _PrismWars._Scripts.Core.Infrastructure.Network {
     public class CaptureTheCatComponent : NetworkBehaviour {
+
         const string CAT_TAG = "Cat";
         const string ZONE_TAG = "Zone";
-        
+
         bool _catPickUpArea = false;
         bool _catPickUped = false;
-        
+
         GameObject _catObj;
-        
+
         HealthComponent _healthComponent;
 
-        public event Action<bool> CatPickUp;
-
         void Start() {
-            if(!IsOwner) return;
+            if (!IsOwner) return;
             _healthComponent = GetComponent<HealthComponent>();
             _healthComponent!.OnDeath += PlayerIsDeath;
         }
 
         void PlayerIsDeath() {
-            if(!IsOwner) return;
-            PlayerDiedServerRpc();
-        }
-
-        [ServerRpc]
-        void PlayerDiedServerRpc() {
+            if (!IsOwner) return;
             _catPickUpArea = false;
             _catObj = null;
         }
-        
-        public void PickUpCat() {
-            if(!IsOwner) return;
-            PickUpCatServerRpc();
-        }
-        [ServerRpc]
-         void PickUpCatServerRpc() {
-            if (_catPickUpArea) {
-                _catPickUped = true;
-                CatIsPickedUpClientRpc(_catPickUped);
-            }
-         }
 
-        [ClientRpc]
-        void CatIsPickedUpClientRpc(bool isPickedUp) {
-            if(IsOwner)
-                CatPickUp?.Invoke(isPickedUp);
+        public void PickUpCat() {
+            if (!IsOwner) return;
+            if (_catPickUpArea) {
+                _catPickUped =  true;
+                ChangeOwnershipServerRpc(_catObj, NetworkManager.Singleton.LocalClientId, true);
+            }
         }
 
         void Update() {
-            if (_catPickUped && IsServer && _catObj) {
+            if (_catPickUped && IsOwner && _catObj) {
                 _catObj.transform.position = gameObject.transform.position + Vector3.up;
             }
         }
 
         void OnTriggerEnter2D(Collider2D other) {
-            if(!IsOwner) return;
+            if (!IsOwner) return;
             other.TryGetComponent(out NetworkObject networkObject);
-            if(networkObject != null) 
-                CatOnTriggerServerRpc(networkObject, true);
-        }
-
-        void OnTriggerExit2D(Collider2D other) {
-            if(!IsOwner) return;
-            other.TryGetComponent(out NetworkObject networkObject);
-            if(networkObject != null) 
-                CatOnTriggerServerRpc(networkObject, false);
-        }
-
-        [ServerRpc]
-        void CatOnTriggerServerRpc(NetworkObjectReference other, bool isEnter = false) {
-            other.TryGet(out NetworkObject cat);
-            if (isEnter && cat != null) {
-                if (cat.CompareTag(CAT_TAG) && cat.gameObject.layer != gameObject.layer) {
+            if (networkObject != null) {
+                if (networkObject.CompareTag(CAT_TAG) && networkObject.gameObject.layer != gameObject.layer) {
                     _catPickUpArea = true;
-                    _catObj = cat.gameObject;
-                }
-            } else if (!isEnter && cat != null) {
-                if (cat.CompareTag(CAT_TAG) && cat.gameObject.layer != gameObject.layer) {
-                    _catPickUpArea = false;
-                    _catObj = null;
+                    _catObj = networkObject.gameObject;
                 }
             }
         }
 
+        void OnTriggerExit2D(Collider2D other) {
+            if (!IsOwner) return;
+            other.TryGetComponent(out NetworkObject networkObject);
+            if (networkObject != null && !_catPickUped) {
+                _catPickUpArea = false;
+                _catObj = null;
+            }
+        }
+
+        [ServerRpc]
+        void ChangeOwnershipServerRpc(NetworkObjectReference other, ulong clientId, bool isEnter = false) {
+            if (isEnter) {
+                other.TryGet(out NetworkObject networkObject);
+                networkObject.ChangeOwnership(clientId);
+            }
+            else {
+                other.TryGet(out NetworkObject networkObject);
+                networkObject.ChangeOwnership(NetworkManager.Singleton.LocalClientId);
+            }
+        }
+        
         void OnDisable() {
             _healthComponent!.OnDeath -= PlayerIsDeath;
         }
