8b553f46e2c0e9ed23c7ac4fdc4ccb96c3641036
rxzv <zxcrxzv@gmail.com>
2025-11-17 12:43:11 +0300

    fix

--- Assets/_PrismWars/_Scripts/Core/Infrastructure/Network/Components/BombCart/BombCartMovement.cs
+++ Assets/_PrismWars/_Scripts/Core/Infrastructure/Network/Components/BombCart/BombCartMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using _PrismWars._Scripts.Game.Player.Model;
 using Unity.Netcode;
@@ -11,32 +12,37 @@ namespace _PrismWars._Scripts.Core.Infrastructure.Network.Components.BombCart {
         [SerializeField] List<Vector3> _fireToIceWaypoints;
         [SerializeField] List<Vector3> _iceToFireWaypoints;
         
-        [SerializeField]NetworkVariable<PlayerElement> _targetElement = new();
-        [SerializeField]NetworkVariable<int> _currentWaypointIndex = new();
+        PlayerElement _targetElement;
         Rigidbody2D _rigidbody;
         NetworkTransform _networkTransform;
         BombCartStateMachine _stateMachine;
+        int _lastFireWaypointIndex;
+        int _lastIceWaypointIndex;
         
-        public PlayerElement CurrentTargetElement => _targetElement.Value;
-        List<Vector3> CurrentWaypoints => _targetElement.Value == PlayerElement.Fire ? 
+        public PlayerElement CurrentTargetElement => _targetElement;
+        List<Vector3> CurrentWaypoints => _targetElement == PlayerElement.Fire ? 
             _fireToIceWaypoints : _iceToFireWaypoints;
+        int CurrentIndexWaypoints => _targetElement == PlayerElement.Fire ? 
+            _lastFireWaypointIndex : _lastIceWaypointIndex;
 
         public override void OnNetworkSpawn() {
             _rigidbody = GetComponent<Rigidbody2D>();
             _stateMachine = GetComponent<BombCartStateMachine>();
             _networkTransform = GetComponent<NetworkTransform>();
             _rigidbody.bodyType = RigidbodyType2D.Dynamic;
+            _lastFireWaypointIndex = 0;
+            _lastIceWaypointIndex = 0;
 
             if(_networkTransform == null) return;
             _networkTransform.Interpolate = true;
             _networkTransform.SyncPositionX = true;
             _networkTransform.SyncPositionY = true;
+            _networkTransform.SyncRotAngleZ = true;
         }
 
         public void SetTargetElement(PlayerElement element) {
             if (!IsServer) return;
-            _targetElement.Value = element;
-            _currentWaypointIndex.Value = 0;
+            _targetElement = element;
         }
 
         public void Move() {
@@ -44,16 +50,32 @@ namespace _PrismWars._Scripts.Core.Infrastructure.Network.Components.BombCart {
             if (CurrentWaypoints == null || CurrentWaypoints.Count == 0) return;
             
             if(_rigidbody.bodyType != RigidbodyType2D.Dynamic) _rigidbody.bodyType = RigidbodyType2D.Dynamic;
-            var currentWaypoint = CurrentWaypoints[_currentWaypointIndex.Value];
+            Vector3 currentWaypoint = CurrentWaypoints[CurrentIndexWaypoints];
             var direction = (currentWaypoint - transform.position).normalized;
             
             _rigidbody.linearVelocity = direction * _moveSpeed;
 
             if(!(Vector2.Distance(transform.position, currentWaypoint) < 0.1f)) return;
-            if (_currentWaypointIndex.Value < CurrentWaypoints.Count - 1) 
-                _currentWaypointIndex.Value++;
-            else if(_currentWaypointIndex.Value == CurrentWaypoints.Count - 1) 
+            if(CurrentIndexWaypoints < CurrentWaypoints.Count - 1) {
+                IncrementLastIndex();
+            }
+            else if(CurrentIndexWaypoints == CurrentWaypoints.Count - 1) 
                 _stateMachine.SetState(_stateMachine.InZoneState);
+        }
+
+        void IncrementLastIndex() {
+            switch (_targetElement) {
+                case PlayerElement.Fire:
+                    _lastFireWaypointIndex++;
+                    break;
+                case PlayerElement.Ice:
+                    _lastIceWaypointIndex++;
+                    break;
+                case PlayerElement.None:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
 
         public void Stop() {
